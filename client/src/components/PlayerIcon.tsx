6c80fd16793eff69b449013d2e6ab1f78b877220
Andy Plank <aplank14@gmail.com>
2024-01-15 20:23:14 -0800

    Optimized svgs

--- client/src/components/PlayerIcon.tsx
+++ client/src/components/PlayerIcon.tsx
@@ -1,19 +1,35 @@
+import bear from '../assets/bear-opt.svg';
+import cat from '../assets/cat-opt.svg';
+import chicken from '../assets/chicken-opt.svg';
+import dog from '../assets/dog-opt.svg';
+import fox from '../assets/fox-opt.svg';
+import lion from '../assets/lion-opt.svg';
+import mouse from '../assets/mouse-opt.svg';
+import panda from '../assets/panda-opt.svg';
+import snake from '../assets/snake-opt.svg';
+import tiger from '../assets/tiger-opt.svg';
+import horse from '../assets/horse-opt.svg';
+import pig from '../assets/pig-opt.svg';
+import sheep from '../assets/sheep-opt.svg';
+import cow from '../assets/cow-opt.svg';
+import monkey from '../assets/monkey-opt.svg';
+
 export const iconMap:any = {
-    bear: "bear.svg",
-    chicken: "chicken.svg",
-    cat: "cat.svg",
-    dog: "dog.svg",
-    fox: "fox.svg",
-    lion: "lion.svg",
-    mouse: "mouse.svg",
-    panda: "panda.svg",
-    snake: "snake.svg",
-    tiger: "tiger.svg",
-    horse: "horse.svg",
-    pig: "pig.svg",
-    sheep: "sheep.svg",
-    cow: "cow.svg",
-    monkey: "monkey.svg",
+    bear: bear,
+    cat: cat,
+    chicken: chicken,
+    dog: dog,
+    fox: fox,
+    lion: lion,
+    mouse: mouse,
+    panda: panda,
+    snake: snake,
+    tiger: tiger,
+    horse: horse,
+    pig: pig,
+    sheep: sheep,
+    cow: cow,
+    monkey: monkey,
 }
 
 const PlayerIcon = ({iconName, customClass}: {iconName: string, customClass?: string}) => {
