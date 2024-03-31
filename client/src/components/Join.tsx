5794215b728cdf0bbe2f96e07c4e12bdd38c5bb9
samanthathompson52 <sthompsonsoftware@gmail.com>
2024-03-31 13:45:31 -0400

    Update styles and functionality in Join component

--- client/src/components/Join.tsx
+++ client/src/components/Join.tsx
@@ -4,6 +4,7 @@ import verifyInput from './InputVerify';
 const Join = ({sendJsonMessage, roomCode, setRoomCode}: {sendJsonMessage: any, roomCode: any, setRoomCode: any}) => {
         
     const [username, setUsername] = useState('');
+    const [type, setType] = useState('');
     
     useEffect(() => {
         const queryParameters = new URLSearchParams(window.location.search)
@@ -14,15 +15,16 @@ const Join = ({sendJsonMessage, roomCode, setRoomCode}: {sendJsonMessage: any, r
     });
 
     const createRoom = () => {
-        sendJsonMessage(({
-            "type": "create_room",
-            "name": username,
-        }));
+        setType("create_room");
     };
 
     const joinRoom = () => {
+        setType("join_room");
+    };
+
+    const enterRoom = () => {
         sendJsonMessage({
-            "type": "join_room",
+            "type": type,
             "name": username,
         });
     };
@@ -40,42 +42,66 @@ const Join = ({sendJsonMessage, roomCode, setRoomCode}: {sendJsonMessage: any, r
     return (
         <div className='grow bg-light-purple justify-center items-center flex'>
             <div>
+                {type !== "" && 
                 <div className='bg-white drop-shadow-lg shadow-sm p-5 md:p-10 text-center m-5 rounded-2xl'>
                     <div className='pb-6'>
-                        <h2>Hello!</h2>
-                        <h3>Let's get started</h3>
+                        <h3>Let's get started!</h3>
                     </div>
                     <div>
-                        <div className="pb-20">
-                            <h4 className='text-left pb-2'>Enter your username:</h4>
+                        <div className="pb-10">
+                            <h4 className='text-left pb-2'>Enter your name:</h4>
                             <input name="username" type="text" aria-label="Username"
                                 value={username} onChange={(e) => usernameInput(e.target.value)} 
                                 className="orange-input" />
                         </div>
-                        <div>
-                            <h4 className='text-left pb-2'>Enter a room code:</h4>
-                            <input name="roomCode" type="text" aria-label="Room code"
-                                value={roomCode} onChange={(e) => roomCodeInput(e.target.value)} 
-                                className="orange-input" />
-                            <div className='pt-4'>
-                                <button 
-                                className='button-orange w-full	focus:ring hover:scale-110 duration-500 p-1 pr-4 pl-4' onClick={joinRoom} >
-                                    <p>Join an existing room</p>
-                                </button>
+                        {type === "join_room" && 
+                            <div className="pb-10">
+                                <h4 className='text-left pb-2'>Enter room code:</h4>
+                                <input name="roomCode" type="text" aria-label="Room code"
+                                    value={roomCode} onChange={(e) => roomCodeInput(e.target.value)} 
+                                    className="orange-input" />
                             </div>
-                        </div>
-                                <p className='italic p-4'>Or</p>
-                        <div className="">
+                        }
+                        <div>
                             <div className='pt-2'>
-                                <button className='button-orange w-full	focus:ring hover:scale-110 duration-500 p-1 pr-4 pl-4' onClick={createRoom} >
-                                    <p>Create a new room</p>
+                                <button className='button-orange w-full focus:ring hover:scale-110 duration-500 p-1 pr-4 pl-4' onClick={enterRoom} >
+                                    <p>Enter Game</p>
                                 </button>
                             </div>
                         </div>
                     </div>
                 </div>
+                }
             </div>
-        </div>
+            <div>
+                {type === "" && 
+                <div className='bg-white drop-shadow-lg shadow-sm p-5 md:p-10 text-center m-5 rounded-2xl'>
+                    <div className='pb-8 border-b-4 border-purple'>
+                        <h3 className='pb-2'>Welcome to Honeypot!</h3>
+                        <h5 className='max-w-md'>The online party game where you answer questions and guess who said what!</h5>
+                    </div>
+                    <div>
+                        <div className='pt-6'>
+                            <div className='pt-4'>
+                                <button 
+                                className='button-orange w-full focus:ring hover:scale-110 duration-500 p-1 pr-4 pl-4' onClick={joinRoom} >
+                                    <h4>Join an existing room</h4>
+                                </button>
+                            </div>
+                        </div>
+                        <p className='italic p-4'>Or</p>
+                        <div className="">
+                            <div className='pt-2'>
+                                <button className='button-orange w-full focus:ring hover:scale-110 duration-500 p-1 pr-4 pl-4' onClick={createRoom} >
+                                    <h4>Create a new room</h4>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                }
+            </div>
+        </div>        
     )
 }
 
